d3ca112827ea09cd1c76bec88463bc78dc37d4f1
mxdavis <md01@me.com>
2018-01-28 15:58:32 +0200

    Great things about me, and pictures

---
+++ src/data/greatThings.js
@@ -0,0 +1,26 @@
+export const greatThings = [
+  {
+    image: "code.png",
+    text: "I love to code, and I'm good at it."
+  },
+  {
+    image: "ideas.png",
+    text: "I'm a fast learner and I'm excited to learn new things."
+  },
+  {
+    image: "team.png",
+    text: "I'm an excellent team player and colleague."
+  },
+  {
+    image: "personality.png",
+    text: "I have a fun personality and great values."
+  },
+  {
+    image: "success.png",
+    text: "I work against the odds. I don't give up until I figure it out."
+  },
+  {
+    image: "reactRuby.png",
+    text: "Ruby on Rails and React/Redux are my specialties."
+  }
+]
