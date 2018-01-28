fd4186229fd04ba5185daacab217b93380df16cb
mxdavis <md01@me.com>
2018-01-28 15:25:45 +0200

    create root App with css and test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('Should render App correctly', () => {
+    const tree = renderer.create(
+        <App />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
 });
